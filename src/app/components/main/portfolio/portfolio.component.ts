cb0d4a3041a0e21b037ebc64102b491b5dd8c9bf
sardar lodhi <sardar.lodhi@piyovi.com>
2024-03-31 11:09:01 +0530

    Enhanced portfolio component

--- src/app/components/main/portfolio/portfolio.component.ts
+++ src/app/components/main/portfolio/portfolio.component.ts
@@ -1,14 +1,60 @@
 import { Component, Input } from '@angular/core';
 import { NxgBannerComponent } from '../../../shared/banner/banner.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'nxg-portfolio',
   standalone: true,
-  imports: [NxgBannerComponent],
+  imports: [CommonModule,RouterOutlet, RouterLink, NxgBannerComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class NxgPortfolioComponent {
   title: string = 'Portfolio';
   @Input() hidden: any;
+  portfolioGellary: any = [
+    {
+      url: '/portfolio/view',
+      src: 'assets/images/portfolio/portfolio1.png',
+      alt: 'portfolio1.png',
+      title: 'Musician',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+    {
+      url: '/',
+      src: 'assets/images/portfolio/portfolio2.png',
+      alt: 'portfolio1.png',
+      title: 'SoftwareM4',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+    {
+      url: '/',
+      src: 'assets/images/portfolio/portfolio3.png',
+      alt: 'portfolio1.png',
+      title: 'Photography',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+    {
+      url: '/',
+      src: 'assets/images/portfolio/portfolio4.png',
+      alt: 'portfolio1.png',
+      title: 'University',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+    {
+      url: '/',
+      src: 'assets/images/portfolio/portfolio5.png',
+      alt: 'portfolio1.png',
+      title: 'Logistics',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+    {
+      url: '/',
+      src: 'assets/images/portfolio/portfolio6.png',
+      alt: 'portfolio1.png',
+      title: 'Logis',
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, deleniti'
+    },
+  ]
 }
---
+++ src/app/components/main/portfolio/view/view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxg-portfolio-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.scss'
+})
+export class NxgPortfolioViewComponent {
+
+}
